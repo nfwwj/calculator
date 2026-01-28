3cb56fbc021259e1027d5ac4818b77afc146f532
nfwwj <wongwenjie1219@gmail.com>
2026-01-28 20:46:27 +0800

    enhanced calculate func

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const buttons = document.querySelectorAll('button')
 const clear = document.querySelector('.clear') 
 const display = document.querySelector('.display')
-
+const calculate = document.querySelector('.calculate')
 // query slector all returns an arr
+let calculated = false
 
 buttons.forEach((button) => button.addEventListener("mouseover", (event)=>{
     button.classList.toggle("hover")
@@ -15,6 +16,27 @@ buttons.forEach((button) => button.addEventListener("mouseleave", (event)=>{
 
 buttons.forEach((button) => button.addEventListener("click", (event)=>{
     let num = button.innerText
+    if (calculated == true){
+        if (button.classList.contains("op")){
+            display.value += num
+            calculated = false
+            return
+
+        }
+        else{
+
+      
+        clearDisplay()
+        display.value = num
+        calculated = false
+        return
+        }  }
+    
+    if (button.innerText == "="){
+        return
+    }
+   
+   
     if (display.value == 0){
         display.value = num
     }
@@ -23,10 +45,31 @@ buttons.forEach((button) => button.addEventListener("click", (event)=>{
     }
 }))
 
-clear.addEventListener("click",()=>{
+
+
+function clearDisplay(){
     display.value = "0"
+}
+
+clear.addEventListener("click",()=>{
+clearDisplay()
 })
 
-
+function calculatefunc(){
+    try{
+    var results = eval(display.value.replaceAll('x','*').replaceAll('÷','/'))
+    if (results == Infinity){
+        throw new Error()
+    }
+    console.log(results)
+    display.value = results
+    calculated = true
+   }
+   catch{
+    display.value = "Error!"
+    calculated = true
+   }
+}
+calculate.addEventListener('click', calculatefunc)
 
 
